4d7b2bb8fd2bf4aa439aff66ef27b251821cbb64
Javed Alimzai <{new_email}>
2025-02-25 12:26:18 -0500

    Update entire project

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -8,7 +8,7 @@ const About = () => {
             <h1>About</h1>
             <p className="about-text">
                 I am a web developer with a passion for web development. 
-                I have been working in the field for over 10 years, and have experience with a variety of technologies. 
+                I have been working in the field for over 1.5 years, and have experience with a variety of technologies. 
                 I have worked on a number of projects, ranging from small websites to large web applications. 
                 I am always looking to learn new things and improve my skills, and I am excited to take on new challenges.
             </p>
